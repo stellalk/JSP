7e1367d3bbe912de15f2fed50ddc3a09c6f81e8f
stellayk <yeeunkk@gmail.com>
2021-04-06 10:44:44 +0900

    04/06/2021 Service

---
+++ ch10/src/main/java/controller/MainContoller.java
@@ -0,0 +1,115 @@
+package controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.CommonService;
+
+public class MainContoller extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private Map<String, Object> instances = new HashMap<>();
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// 컨트롤러가 최초 실행될때 실행되는 초기화 메서드  
+		
+		// 프로퍼티 파일(액션주소 맵핑 파일) 경로 구하기
+		ServletContext ctx = config.getServletContext();
+		String path = ctx.getRealPath("/WEB-INF")+"/urlMapping.properties";
+		
+		// 프로퍼티 파일 입력 스트림 연결
+		Properties prop = new Properties();
+		
+		try {
+			
+			FileInputStream fis = new FileInputStream(path);
+			prop.load(fis);
+			fis.close();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 프로퍼티 객체 생성 및 Service 객체 생성후 보관
+		Iterator iter = prop.keySet().iterator();
+		
+		while(iter.hasNext()) {
+			
+			String k = iter.next().toString();
+			String v = prop.getProperty(k);
+			
+			try {
+				Class obj = Class.forName(v);
+				Object instance = obj.newInstance();
+				
+				instances.put(k, instance);
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		requestProc(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		requestProc(req, resp);
+	}
+	
+	public void requestProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 요청 주소에서 service 객체 key 구하기
+		String path = req.getContextPath();
+		String uri  = req.getRequestURI();
+		String key = uri.substring(path.length());
+		
+		// service 객체 map에서 꺼내기
+		CommonService instance = (CommonService) instances.get(key);		
+		
+		// service 객체 실행후 view 리턴 받기
+		String view = instance.requestProc(req, resp);
+	
+		// View 포워드
+		RequestDispatcher dispatcher = req.getRequestDispatcher(view);
+		dispatcher.forward(req, resp);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
